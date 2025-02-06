e8de3663f6a4ff71af309b7384c71c75d510780a
Patrick <patrickmassesson@gmail.com>
2025-02-06 13:09:27 +0100

    done

--- dadjoke.js
+++ dadjoke.js
@@ -1,7 +1,9 @@
-fetch ("https://icanhazdadjoke.com/", {
+fetch("https://icanhazdadjoke.com/", {
     headers: {
         "accept": "application/json"
     }
 })
 .then(response => response.json())
-.then(data => console.log(data))
+.then(data => {
+    document.querySelector(".joke").innerText = data.joke;
+})
